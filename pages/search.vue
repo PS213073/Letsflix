ebcfd94bdcf892fd084078a553a9da850f1725c3
Daniel Shahzada <107118950+PS213073@users.noreply.github.com>
2024-06-22 19:17:10 +0200

    search page tv-shows card grid fixed

--- pages/search.vue
+++ pages/search.vue
@@ -62,7 +62,7 @@
         TV Series Results
       </h1>
       <ul
-        class="grid justify-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4"
+        class="grid justify-items-center space-y-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5"
       >
         <li v-for="tvSeries in series" :key="tvSeries.id">
           <SeriesCard
